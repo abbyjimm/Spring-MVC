017b41d5878d44ed2c75f1cb6c8c7a760402a3f0
Aby <aby.jimm@gmail.com>
2019-11-25 21:43:11 -0600

    Binding Request Params

--- src/com/code/springdemo/mvc/HelloWorldController.java
+++ src/com/code/springdemo/mvc/HelloWorldController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -23,7 +24,7 @@ public class HelloWorldController {
 	
 	// Method to read data from the form
 	// Using model to add data
-	@RequestMapping("/processFormVersionTwo")
+	@RequestMapping("/processFormVersionModel")
 	public String recoveringDataStudent(HttpServletRequest request, Model model) {
 		
 		// Read the request parameter from the HTML form
@@ -40,5 +41,21 @@ public class HelloWorldController {
 		
 		return "helloworld";
 	}
-
+	
+	// Method to read data from the form with Annotation RequestParam to specify form attribute 
+	// Using model to add data
+	@RequestMapping("/processFormVersionAnnotationParameter")
+	public String recoveringDataStudentByAnnotation(@RequestParam("studentName") String name, Model model) {
+		
+		// Convert the data 
+		name = name.toUpperCase();
+		
+		// Create a message
+		String result = "Hello " + name;
+		
+		// Add message to the Model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
 }
